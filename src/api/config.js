d2a4e7f622058f10b518f94eb707cf35d842c750
Nanoparty <nfootega@gmail.com>
2021-05-31 00:57:24 -0400

    api key folder

---
+++ src/api/config.js
@@ -0,0 +1,1 @@
+export const apiKey = "RuNwRut6syrdoLMirt1OHZT3_upvFTMkYy5RyvxzAyo";
--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 import './App.css';
-const API_KEY = process.env.REACT_APP_API_KEY
+import { apiKey } from '../api/config'
 
 class App extends React.Component  {
     state = { images: [] };
@@ -13,7 +13,7 @@ class App extends React.Component  {
         const response = await axios.get('https://api.unsplash.com/search/photos?page=1&per_page=100', {
             params: { query: term},
             headers: {
-                Authorization: 'Client-ID RuNwRut6syrdoLMirt1OHZT3_upvFTMkYy5RyvxzAyo'
+                Authorization: `Client-ID ${apiKey}`
             }
         })
 
